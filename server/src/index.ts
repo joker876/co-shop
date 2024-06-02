1c75b2b3a393005c8f8587461f38372b7e838dd9
joker876 <joker876x@gmail.com>
2024-06-02 21:32:45 +0200

    [s] add console coloring

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
-import { createApp } from "./createApp";
+import ansis from 'ansis';
+import { createApp } from './createApp';
+import { ERROR_STR, INFO_STR, OK_STR } from './utils/console-colors';
 
 const app = createApp();
 
 const PORT = 3000;
 
+console.log(`Starting server...`);
+
 app.listen(PORT, () => {
-	console.log(`Running on Port ${PORT}`);
+    console.log(`${OK_STR}Running on port ${ansis.greenBright.underline(PORT.toString())}!`);
 });
