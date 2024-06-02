8a76d7633dffc50cce85cd1208bd6d53f602383d
joker876 <joker876x@gmail.com>
2024-06-02 23:14:57 +0200

    setup dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
 import ansis from 'ansis';
 import { createApp } from './createApp';
-import { ERROR_STR, INFO_STR, OK_STR } from './utils/console-colors';
+import { OK_STR } from './utils/console-colors';
+import 'dotenv/config';
 
 const app = createApp();
 
-const PORT = 3000;
+const PORT = process.env.PORT ?? 8080;
 
 console.log(`Starting server...`);
 
 app.listen(PORT, () => {
-    console.log(`${OK_STR}Running on port ${ansis.greenBright.underline(PORT.toString())}!`);
+  console.log(`${OK_STR}Running on port ${ansis.greenBright.underline(String(PORT))}!`);
 });
