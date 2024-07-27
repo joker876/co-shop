5b00e4c7bd420769f59c706137b08c5a47f9c5fa
joker876 <joker876x@gmail.com>
2024-07-27 15:46:56 +0200

    create basic main page layout

---
+++ client/src/app/features/main-page/main.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.page.html',
+  styleUrl: './main.page.scss'
+})
+export class MainPage {
+
+}
